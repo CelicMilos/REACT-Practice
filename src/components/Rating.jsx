4dc98637d1366fd16ca72f2608bc9b64139a4d3c
CelicMilos <milos.celic.b@gmail.com>
2025-10-02 14:52:25 +0200

    Stars added

---
+++ src/components/Rating.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+const Rating = () => {
+    const [rating,setRating]=useState(0)
+    const [hover,setHover]=useState(0)
+    const feedbackMessages=['Terible',"Poor",'Fair','Good','Excellent'];
+    const stars=Array.from({length:5},(_,i)=>i+1)
+    const clicked =(index)=>console.log('Clicked',index);
+    const hovered=(direction,index)=>console.log('Hovered!',direction,index);
+    return ( 
+    <div className='rating-container' style={styles.container}>
+        <h2 style={styles.heading}> Rate Your Expirience</h2>
+        <div className="stars">
+            {stars.map((star,index)=>(
+                <span 
+                onClick={()=>setRating(star)}
+                onMouseEnter={()=>setHover(star)} 
+                onMouseLeave={()=>setHover(0)} 
+                key={star}
+                className={`star ${star <=(hover||rating) ? 'active':''}`}
+                >
+                {'\u2605'}
+                </span>
+            ))}
+            {/* {hover} */}
+        </div>
+        {rating >0 && <p className ='feedback'>{feedbackMessages[rating -1]}</p>}
+       </div> );
+
+}
+
+const styles={
+    container:{
+        textAlign:'center',
+        fontFamily:'Arial',
+        padding:'20px',
+    },
+    heading:{color:'red'}
+}
+
+export default Rating;
+// Stilizivanje moze ovako a moze  i u jednoj liniji pored div taga ali moraju{{}}
+// jer je to u sustini JS objekat
+// {/* <div style={{
+//       textAlign:'center',
+//         fontFamily:'Arial',
+//         padding:'20px',
+// }}>
+//   */}
+// A za manje projekte moze da se korist globalni .css fajl
