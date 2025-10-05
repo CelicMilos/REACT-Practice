1ecabf24626bc8eb164c59783e2540a1c36a1224
CelicMilos <165153757+CelicMilos@users.noreply.github.com>
2025-10-05 20:11:55 +0200

    Add files via upload

---
+++ Rating UI Mini Project/src/components/Star.jsx
@@ -0,0 +1,11 @@
+const Star = ({star,rating,hover,color,ratingClick,hoverEnter,hoverLeave}) => {
+    return <span
+        onClick={()=> ratingClick(star)} 
+        onMouseEnter={()=>hoverEnter(star)}
+        onMouseLeave={hoverLeave}
+        className={`star ${star <=(hover||rating) ? 'active':''}`}
+        style={{color:star<=(hover||rating) ? color:"#ccc"}}
+        >{'\u2605'}</span>;
+}
+ 
+export default Star;
